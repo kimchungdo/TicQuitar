bc0ab39034f8328e4d9d89b97dc50aa7f912cf42
Chung-Do Kim <dorian21@naver.com>
2021-12-03 16:53:48 +0900

    GyeGame is updated
    should make it randomly and its UI must be edited...

--- lib/games/gye_name_game_page.dart
+++ lib/games/gye_name_game_page.dart
@@ -13,9 +13,18 @@ class GyeNameGamePage extends StatefulWidget {
 class _GyeNameGamePageState extends State<GyeNameGamePage> {
   bool version = true;
   List<String> quiz = [];
+  Color basicColor = Colors.yellow;
 
   int gyeKey = Random().nextInt(7);
   //version ? quiz = korGye : quiz = engGye;
+
+  void getNextGye(){
+    setState(() {
+      gyeKey = Random().nextInt(7);
+      basicColor = Colors.yellow;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -27,20 +36,52 @@ class _GyeNameGamePageState extends State<GyeNameGamePage> {
             Container(
               child: Center(
                   child: Text("${korGye[gyeKey]}", style: TextStyle(fontSize: 56))),
-              color: Colors.yellow,
+              color: basicColor,
               height: 100,
             ),
 
             Container(height: 30),
 
-            ElevatedButton(onPressed: (){}, child: Text("${engGye[0]}")),
+/*            ElevatedButton(onPressed: (){}, child: Text("${engGye[0]}")),
             ElevatedButton(onPressed: (){}, child: Text("${engGye[1]}")),
             ElevatedButton(onPressed: (){}, child: Text("${engGye[2]}")),
             ElevatedButton(onPressed: (){}, child: Text("${engGye[3]}")),
             ElevatedButton(onPressed: (){}, child: Text("${engGye[4]}")),
             ElevatedButton(onPressed: (){}, child: Text("${engGye[5]}")),
-            ElevatedButton(onPressed: (){}, child: Text("${engGye[6]}")),
+            ElevatedButton(onPressed: (){}, child: Text("${engGye[6]}")),*/
 
+            GridView.count(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              children: List.generate(engGye.length, (_idx){
+                return InkWell(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text(engGye[_idx], style: TextStyle(fontSize: 20)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Color(0xFFff7f7f)
+                    ),
+                  ),
+                  onTap: (){
+                    if(_idx == gyeKey){
+                      print("정답");
+                      getNextGye();
+                    }
+                    else{
+                      setState(() {
+                        basicColor = Colors.red;
+                      });
+                    }
+
+                    },
+                );
+              }),
+              crossAxisCount: 3,
+              crossAxisSpacing: 4,
+              mainAxisSpacing: 4,
+              childAspectRatio: 1,
+            )
           ],
         ));
   }
