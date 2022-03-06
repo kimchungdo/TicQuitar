ba5e5bd23c6959b2156eda3a146726d2cf104193
Chung-Do Kim <dorian21@naver.com>
2022-03-07 00:49:03 +0900

    add youtube plugin

--- lib/page/goal_tab/goal_page.dart
+++ lib/page/goal_tab/goal_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tic_quitar_2/components/text.dart';
-
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 class GoalPage extends StatefulWidget {
   const GoalPage({Key? key}) : super(key: key);
 
@@ -9,6 +9,22 @@ class GoalPage extends StatefulWidget {
 }
 
 class _GoalPageState extends State<GoalPage> {
+  String? videoId = YoutubePlayer.convertUrlToId("https://www.youtube.com/watch?v=BBAyRBTfsOU");
+  TextEditingController linkController = TextEditingController();
+
+  Widget buildInputField() => Container(
+    child: TextField(
+      controller: linkController,
+      decoration: InputDecoration(
+        hintText: "Input Youtube Link Please",
+        suffixIcon: Icon(Icons.search),
+      ),
+    )
+  );
+
+  Widget buildItem() => Container(
+    child: Text("hello"),
+  );
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,6 +32,74 @@ class _GoalPageState extends State<GoalPage> {
       child: Column(
         children: [
           buildText(text: "My Song Link", size: 24, weight: Weight.Bold),
+          buildInputField(),
+          TextButton(onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Player(videoId!,'hello'))
+            );
+          }, child: Text("clickme"))
+        ],
+      ),
+    );
+  }
+}
+
+
+class Player extends StatefulWidget {
+  final String _videoID;
+  final String _videoTitle;
+
+  Player(this._videoID, this._videoTitle);
+
+  @override
+  PlayerState createState() => PlayerState(_videoID, _videoTitle);
+}
+
+class PlayerState extends State<Player> {
+  String _videoID;
+  String _videoTitle;
+
+  PlayerState(this._videoID, this._videoTitle);
+
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    _controller = YoutubePlayerController(
+      initialVideoId: _videoID,
+      flags: const YoutubePlayerFlags(
+        mute: false,
+        autoPlay: false,
+        disableDragSeek: false,
+        loop: false,
+        isLive: false,
+        forceHD: false,
+        enableCaption: true,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '$_videoTitle',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
+      body: YoutubePlayer(
+        key: ObjectKey(_controller),
+        controller: _controller,
+        actionsPadding: const EdgeInsets.only(left: 16.0),
+        bottomActions: [
+          CurrentPosition(),
+          const SizedBox(width: 10.0),
+          ProgressBar(isExpanded: true),
+          const SizedBox(width: 10.0),
+          RemainingDuration(),
+          //FullScreenButton(),
         ],
       ),
     );
